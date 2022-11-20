93e3f401ca3106a8a36e78f5bd67979b82804f5e
Max Nowak <spearekurwa@yandex.com>
2022-11-20 21:14:54 +0300

    add: hello function (3)

--- change_mac_adress/src/main.py
+++ change_mac_adress/src/main.py
@@ -3,14 +3,25 @@
 from subprocess import SubprocessError, call
 from rich.progress import track
 from time import sleep
+from rich.console import Console
+from rich.panel import Panel
+from rich import print
 import argparse
+console = Console()
 
+def hello():
+    console.print(Panel('''
+    Help --> python3 main.py [red]--hello[/red]
+    How it should look like --> python3 main.py -i [red]INTERFACE[/red] -m [red]NEW_MAC[/red]
+    For example --> [blue]python3 main.py -i eth0 -m 00:11:22:33:44:55[/blue]
+    Check result --> [red]ifconfig
+    ''', title='[white]Change MAC address'), justify='center')
 
 def get_arguments():
     parser = argparse.ArgumentParser(
         prog='MacAddress',
         description='The programm will got changed you MAC Address.',
-        epilog='Check it in "ifconfig"')
+        epilog='HOW IT SHOULD LOOK LIKE --> python3 main.py -i eth0 -m 00:11:22:33:44:55')
     parser.add_argument('-i',  '--interface',
                         help='Interface to change its MAC address')
     parser.add_argument('-m', '--mac', help='New MAC address')
@@ -29,12 +40,11 @@ def change_mac(interface, new_mac):
     call(['ifconfig', interface, 'down'])
     call(['ifconfig', interface, 'hw', 'ether', new_mac])
     call(['ifconfig', interface, 'up'])
-    call(['ifconfig'])
 
 
-options, argument = get_arguments(), get_arguments()
+# options, argument = get_arguments(), get_arguments()
 
-change_mac(options.interface, options.mac)
+# change_mac(options.interface, options.mac)
 
-if '__name__' == '__main__':
-    get_arguments()
+if __name__ == '__main__':
+    hello()
